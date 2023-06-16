c8eb408b0741b5dd182c15f4f66a3517e187c19a
Liam Collod <lcollod@gmail.com>
2023-06-16 10:24:00 +0200

    [_numbered] add jump to line shortcut (ctrl+g)

--- lqtTextEditor/_numbered.py
+++ lqtTextEditor/_numbered.py
@@ -1,8 +1,51 @@
+import logging
 from typing import Optional
 
 from Qt import QtGui
 from Qt import QtCore
 from Qt import QtWidgets
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class JumpToLineDialog(QtWidgets.QDialog):
+    """
+    A small dialog that prompt the user a line number to jump to.
+
+    Args:
+        max_lines: maximal number of lines in the parent editor
+    """
+
+    def __init__(self, max_lines: int, parent=None):
+        super().__init__(parent=parent)
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.layout_center = QtWidgets.QHBoxLayout()
+        self.label = QtWidgets.QLabel("Line Number:")
+        validator = QtGui.QIntValidator(1, max_lines, self)
+        self.line_edit = QtWidgets.QLineEdit()
+        self.buttons = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.Ok | QtWidgets.QDialogButtonBox.Cancel
+        )
+
+        self.setLayout(self.layout)
+        self.layout.addLayout(self.layout_center)
+        self.layout.addWidget(self.buttons)
+        self.layout_center.addWidget(self.label)
+        self.layout_center.addWidget(self.line_edit)
+
+        self.setWindowTitle("Jump To Line")
+        self.line_edit.setValidator(validator)
+        self.line_edit.setText(str(max_lines))
+        self.line_edit.setFocus(QtCore.Qt.PopupFocusReason)
+
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+
+    @property
+    def line_number(self) -> int:
+        return int(self.line_edit.text() or "1")
 
 
 class LineSideBarWidget(QtWidgets.QWidget):
@@ -192,6 +235,13 @@ class LinePlainTextEdit(QtWidgets.QPlainTextEdit):
         self._update_sidebar_geo()
         self._update_margins()
 
+    def _on_jump_to_line(self):
+        dialog = JumpToLineDialog(max_lines=self.blockCount())
+        result = dialog.exec_()
+        if result != dialog.Accepted:
+            return
+        self.jump_to_line(dialog.line_number - 1)
+
     def _on_selection_changed(self):
         """
         Callback when this text cursor change.
@@ -303,6 +353,17 @@ class LinePlainTextEdit(QtWidgets.QPlainTextEdit):
             if cursor.selectedText() == self._tab_character:
                 cursor.removeSelectedText()
 
+    def jump_to_line(self, line_number: int):
+        """
+        Set the cursor (and the viewport) active on the given line number.
+        """
+        block = self.document().findBlockByNumber(line_number)
+        if not block.isVisible():
+            LOGGER.warning(f"Cannot jump to line {line_number}: line is hided.")
+            return
+        cursor = QtGui.QTextCursor(block)
+        self.setTextCursor(cursor)
+
     def set_left_margin(self, margin: int):
         """
         Set the margin size for the left side of the viewport.
@@ -374,6 +435,12 @@ class LinePlainTextEdit(QtWidgets.QPlainTextEdit):
         elif event.key() == QtCore.Qt.Key_Tab:
             self._indent_selection()
             return
+
+        elif (
+            event.key() == QtCore.Qt.Key_G
+            and event.modifiers() & QtCore.Qt.ControlModifier
+        ):
+            self._on_jump_to_line()
 
         super().keyPressEvent(event)
 
